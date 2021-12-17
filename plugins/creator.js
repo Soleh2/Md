d8f017ecd67d1e353ddebfab64608f89f11143cb
Soleh2 <90469140+Soleh2@users.noreply.github.com>
2021-12-17 10:54:52 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,9 +5,9 @@ async function handler(m) {
                     + 'N:;Adul Alhy;;;'
                     + 'FN:Adul Alhy\n' // full name
                     + 'ORG:Adul Alhy;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6281390199407:+62 813-9019-9407\n' // WhatsApp ID + phone number
+                    + 'TEL;type=CELL;type=VOICE;waid=6281390199407:+62 812-5284-8955\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Adul Alhy', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Nando', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
