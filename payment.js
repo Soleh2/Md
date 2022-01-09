071d94dd5da24c689afbd8f09bb3475c923b5109
Soleh2 <90469140+Soleh2@users.noreply.github.com>
2022-01-09 20:51:09 +0700

    Update payment.js

--- payment.js
+++ payment.js
@@ -3,11 +3,12 @@ let nando = 'https://telegra.ph/file/1a215aee5e7dfcaf08198.jpg'
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(nando)).buffer(), `
 *LIST PAYMENT BANK & WALLET*
 
-✅ *DANA:* 082210295366 AN. Ratna Susilawati
-✅ *SHOPEEPAY:* 082210295366 AN.Rishakii       
+✅ *DANA:* 082210295366 An. Ratna Susilawati
+✅ *SHOPEEPAY:* 082210295366 An.Rishakii       
 ✅ *QRIS:* MINTA PC ADMIN
+✅ *OVO:* 082210295366 An. MUHAMMAD CHAIRI
 ✅ *GOPAY:* 082210295366 An. Ratna Susilawati
-✅ *BANK:* Mandiri: 1770015710608 AN. Risna Ari Fitria
+✅ *BANK:* Mandiri 1770015710608 An. Risna Ari Fitria
 `.trim(), watermark, 'Kembali Ke list', '.dmml1', m)
 handler.help = ['payment']
 handler.command = /^(payment)$/i
